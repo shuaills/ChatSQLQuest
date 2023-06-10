be14f9909601fad9103e8a78bb4860958d021dd8
shuaills <shishuaiuoe@gmail.com>
2023-06-10 14:22:08 +0800

    Data Loader Done

--- DataLoader/gradio_data_loader.py
+++ DataLoader/gradio_data_loader.py
@@ -1,34 +1,28 @@
 import os
 import gradio as gr
+from shutil import copyfile
+
 
 class GradioDataLoader:
     def __init__(self, upload_folder_path: str):
         self.upload_folder_path = upload_folder_path
 
-    def save_uploaded_file(self, uploaded_file):
+    def save_uploaded_file(self, uploaded_file, original_filename):
         # Get file metadata
-        filename = uploaded_file.name
-        file_type = os.path.splitext(filename)[-1].lstrip('.').lower()
+        file_type = os.path.splitext(original_filename)[-1].lstrip('.').lower()
 
         if file_type not in ["csv", "xlsx", "sql"]:
             return f"Unsupported file type: {file_type}"
 
         try:
             # Define destination file path
-            dest_file_path = os.path.join(self.upload_folder_path, filename)
+            dest_file_path = os.path.join(self.upload_folder_path, original_filename)
 
             # Save the uploaded file to the destination file path
             with open(dest_file_path, 'wb') as f:
                 f.write(uploaded_file.read())
+                
 
             return f"File saved successfully to {dest_file_path}"
         except Exception as e:
             return f"Error occurred: {e}"
-
-def process_upload(uploaded_file):
-    upload_folder_path = 'UploadedFile'  # This should be your uploaded file folder path
-    data_loader = GradioDataLoader(upload_folder_path)
-    return data_loader.save_uploaded_file(uploaded_file)
-
-iface = gr.Interface(fn=process_upload, inputs="file", outputs="text")
-iface.launch()
--- app.py
+++ app.py
@@ -1,16 +1,39 @@
-from io import StringIO
 import gradio as gr
-import random
-import time
 import pandas as pd
 from DataLoader.sql_data_loader import *
 from DataLoader.gradio_data_loader import *
+from shutil import copyfile
 
 
-def process_upload(uploaded_file):
-    upload_folder_path = 'UploadedFile'  # This should be your uploaded file folder path
+
+def process_uploads(uploaded_files):
+    root_dir = os.path.dirname(os.path.abspath(__file__))  # Get the root directory of your project
+    upload_folder_path = os.path.join(root_dir, 'UploadedFile')  # Construct the path to the "UploadedFile" folder
+    print(upload_folder_path)
+
+    # Check if the "UploadedFile" folder exists, if not, create it
+    if not os.path.exists(upload_folder_path):
+        os.mkdir(upload_folder_path)
+
     data_loader = GradioDataLoader(upload_folder_path)
-    return data_loader.save_uploaded_file(uploaded_file)
+    processed_files = []
 
-iface = gr.Interface(fn=process_upload, inputs="file", outputs="text")
+    for uploaded_file in uploaded_files:
+        original_filename = os.path.basename(uploaded_file.name)
+        print(original_filename)
+        data_loader.save_uploaded_file(uploaded_file, original_filename)
+        processed_files.append(original_filename + " has been processed. /n")
+
+    return processed_files
+
+
+
+# Create a Gradio interface
+iface = gr.Interface(
+    fn=process_uploads,
+    inputs = gr.Files(),
+    outputs="text"
+)
+
+# Launch the Gradio interface
 iface.launch()
